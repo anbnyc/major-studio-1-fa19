4ed8bbe87091272b3885311d74a698ff2bb781e4
readyletsgo <rich@rt80.net>
2019-10-02 16:55:28 -0400

    images example: node json prep and download, vibrant.js example

--- lab6_images/node_image_download/node_download_images.js
+++ lab6_images/node_image_download/node_download_images.js
@@ -1,8 +1,12 @@
-let fs = require('fs'),
-    request = require('request');
+// load a default library that lets us read/write to the file system
+let fs = require('fs');
+// load a default library that lets us make HTTP requests (like calls to an API)
+let request = require('request');
 
+// the folder we will write into
 let folder = "downloads";
 
+// download the image by url, name the file by filename
 function downloadImage(uri, filename, callback){
   request.head(uri, function(err, res, body){
     // console.log('content-type:', res.headers['content-type']);
@@ -11,13 +15,12 @@ function downloadImage(uri, filename, callback){
   });
 };
 
+// go through the json we created before
 function downloadData() {
   fs.readFile("./data.json", "utf8", (err, data) => {
     if (err) console.log(err);
-    console.log(JSON.parse(data));
 
     JSON.parse(data).forEach(e => {
-      // let filename =  e.primaryImage.split('/').pop();
       console.log('Downloading ' + e.filename);
       downloadImage(e.primaryImage, e.filename, function(){
         console.log('Finished Downloading ' + e.filename);
