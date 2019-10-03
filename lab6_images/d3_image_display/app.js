9a5078df33143a279b15f49e2626e8b3d59e870b
Zach Krall <zach@zachkrall.com>
2019-10-03 14:41:19 -0400

    added d3 image example week 6

---
+++ lab6_images/d3_image_display/app.js
@@ -0,0 +1,20 @@
+(async () => {
+
+let data = await fetch('./data.json').then( res => res.json() ); 
+let app  = d3.select('#app');
+
+let card = app.selectAll('div.met-image')
+              .data(data)
+              .join('div')
+              .attr('class', 'met-image');
+
+card.append('div')
+    .attr('class', 'img-card')
+    .append('img')
+    .attr('src', (d)=>{ return 'images/' + d.filename} )
+    .attr('width', '200');
+
+card.append('h1')
+    .text((d)=>{ return d.title});
+
+})();
