59735ca78fae73e06b72e6e0d9f563812f02e830
readyletsgo <rich@rt80.net>
2019-11-07 18:23:46 -0500

    p5 tween example

---
+++ lab10_motion/p5_tween/sketch.js
@@ -0,0 +1,25 @@
+// const position = { x : random(width), y: random(height) };
+let position, target, tween;
+
+function setup() {
+ 	createCanvas(800,600);
+  background(0);
+  fill(255);
+  noStroke();
+  position = { x : random(width), y: random(height) };
+}
+
+
+function draw(){
+  background(0, 50);
+  TWEEN.update();
+  ellipse(position.x, position.y,30);
+}
+
+function mouseReleased() {
+  target = { x : mouseX, y: mouseY };
+  tween = new TWEEN.Tween(position).to(target, 400);
+  tween.easing(TWEEN.Easing.Sinusoidal.InOut);
+
+    tween.start();
+}
